47da764d1724b83e955745740fed0bc3c9e0da3d
elishayer <eshayer@stanford.edu>
2016-04-12 22:48:50 -0700

    Seward's Folly Random

---
+++ src/main/java/org/ggp/base/player/gamer/statemachine/sfrandom/SfRandomGamer.java
@@ -0,0 +1,71 @@
+package org.ggp.base.player.gamer.statemachine.sfrandom;
+
+import java.util.List;
+import java.util.Random;
+
+import org.ggp.base.apps.player.detail.DetailPanel;
+import org.ggp.base.apps.player.detail.SimpleDetailPanel;
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public final class SfRandomGamer extends StateMachineGamer
+{
+	@Override
+	public String getName() {
+		return "SewardsFollyRandom";
+	}
+
+	@Override
+    public StateMachine getInitialStateMachine() {
+        return new CachedStateMachine(new ProverStateMachine());
+    }
+
+	@Override
+    public DetailPanel getDetailPanel() {
+        return new SimpleDetailPanel();
+    }
+
+	@Override
+    public void stateMachineAbort() {
+        // Sewards Folly's Random Gamer does no special cleanup when the match ends abruptly.
+    }
+
+    @Override
+    public void stateMachineStop() {
+        // Sewards Folly's Random Gamer does no special cleanup when the match ends normally.
+    }
+
+    @Override
+    public void preview(Game g, long timeout) throws GamePreviewException {
+        // Sample gamers do no game previewing.
+    }
+
+	@Override
+	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+    {
+        // No metagraming in the random gamer
+    }
+
+    @Override
+    public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+    {
+        long start = System.currentTimeMillis();
+
+        List<Move> moves = getStateMachine().findLegals(getRole(), getCurrentState());
+        Move randMove = moves.get(new Random().nextInt(moves.size()));
+
+        long stop = System.currentTimeMillis();
+
+        notifyObservers(new GamerSelectedMoveEvent(moves, randMove, stop - start));
+        return randMove;
+    }
+}
